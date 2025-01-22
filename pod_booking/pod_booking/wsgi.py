b3c0fdf9c80b03c36f30081297e9dd8ffde31e6c
nguyenthanhhieu23 <hieunt0471@ut.edu.vn>
2025-01-22 14:00:52 +0700

    mk
    nn

--- pod_booking/pod_booking/wsgi.py
+++ pod_booking/pod_booking/wsgi.py
@@ -14,3 +14,4 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pod_booking.settings')
 
 application = get_wsgi_application()
+gthlknrgjrl3jghi3
