e030209b791146b4e71d91ea20cd93fd2c3c4255
QuangHung <hung82390@gmail.com>
2025-02-07 22:26:56 +0700

    fix

--- home/models.py
+++ home/models.py
@@ -15,7 +15,7 @@ class BaseModel(models.Model):
         ordering=['uid']
 
 class UserProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     phone = models.CharField(max_length=15, blank=True, null=True)
     address = models.TextField(blank=True, null=True)
     
